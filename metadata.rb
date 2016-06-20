0ddc533ea4b43d32cf9ef956da2ec87c1b33455f
Alexey Kaytaz <alexey@kaytaz.ru>
2016-06-20 14:31:38 +0300

    240

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'alexey@kaytaz.ru'
 license 'all_rights'
 description 'Installs/Configures test_proj'
 long_description 'Installs/Configures test_proj'
-version '2.4.0'
+version '2.5.0'
 
 depends 'chef-sugar'
 depends 'firewall', '~> 2.4.0'
