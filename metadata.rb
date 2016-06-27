085ab481fcab5d5d0bc2132f44e09aa405006e1b
Artemii Kropachev <bellintegratordemo>
2016-06-27 10:20:10 -0400

    a

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'alexey@kaytaz.ru'
 license 'all_rights'
 description 'Installs/Configures test_proj'
 long_description 'Installs/Configures test_proj'
-version '2.8.12'
+version '2.8.13'
 
 depends 'chef-sugar'
 depends 'firewall', '~> 2.4.0'
