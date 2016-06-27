9b45f2d2746b68c1f93ad256df2f79243bf23799
Artemii Kropachev <bellintegratordemo>
2016-06-27 08:36:19 -0400

    fixed problem. green application

--- files/default/index.php
+++ files/default/index.php
@@ -10,13 +10,12 @@
         padding: 5px;
       }
       table tr:nth-child(even) td {
-        background-color: #95c7ea;
+        background-color: #00ff00;
       }
     </style>
 </head>
 <body>
 <?php
-ARTEMII
   require_once('customer.php');
   echo "<table>\n";
   foreach (get_sample_customers() as $customer) {
