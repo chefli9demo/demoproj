2dbe922c53f7298448fe00e3e293e1a2206483a8
Artemii Kropachev <bellintegratordemo>
2016-07-01 13:44:34 -0400

    aaa

--- metadata.rb
+++ metadata.rb
@@ -6,11 +6,10 @@ description 'Installs/Configures test_proj'
 long_description 'Installs/Configures test_proj'
 version '2.8.19'
 
-depends 'chef-sugar'
 depends 'firewall', '~> 2.4.0'
 depends 'chef-sugar', '>= 0.0.0'
-depends 'firewall', '~> 2.4.0'
 depends 'httpd', '~> 0.3.4'
 depends 'mysql', '~> 7.1.1'
 depends 'mysql2_chef_gem', '~> 1.1.0'
 depends 'database', '~> 5.1.2'
+
